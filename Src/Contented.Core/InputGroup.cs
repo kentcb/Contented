e82dd5f648bb5b43ff1bf2e55f27d6e6637cd77a
Kent Boogaart <kent.boogaart@gmail.com>
2016-12-28 14:36:09 +1030

    Extract InputGroup

---
+++ Src/Contented.Core/InputGroup.cs
@@ -0,0 +1,82 @@
+﻿namespace Contented.Core
+{
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+
+    [DebuggerDisplay("{Target}")]
+    public sealed class InputGroup
+    {
+        private readonly string target;
+        private readonly IImmutableList<string> uris;
+
+        public InputGroup(
+            string target,
+            IImmutableList<string> uris)
+        {
+            this.target = target;
+            this.uris = uris;
+        }
+
+        public string Target => this.target;
+
+        public string TargetParent
+        {
+            get
+            {
+                var pathParts = GetPathParts(this.target);
+                return pathParts
+                    .Take(pathParts.Count - 1)
+                    .Aggregate("", (acc, next) => acc + "/" + next);
+            }
+        }
+
+        public string TargetName => GetPathParts(this.target).LastOrDefault();
+
+        public IImmutableList<string> Uris => this.uris;
+
+        public static InputGroup Parse(StreamReader reader)
+        {
+            var line = default(string);
+            var target = default(string);
+            var uris = new List<string>();
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    continue;
+                }
+
+                target = line;
+                break;
+            }
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    break;
+                }
+
+                uris.Add(line);
+            }
+
+            if (target == null)
+            {
+                return null;
+            }
+
+            return new InputGroup(
+                target,
+                uris.ToImmutableList());
+        }
+
+        private static IImmutableList<string> GetPathParts(string path) =>
+            path
+                .Split('/', '\\')
+                .ToImmutableList();
+    }
+}
